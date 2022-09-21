27cb36a0a8d894a6ac43b29f46e0b6a5008c4f02
Grant Moore <625518@bah.com>
2022-09-21 14:36:29 -0400

    update provider template and lock versions

--- provider.tf
+++ provider.tf
@@ -21,6 +21,14 @@ terraform {
       source = "hashicorp/time"
       version = "~>0.7.2"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.6.0"
+    }
   }
 }
 
